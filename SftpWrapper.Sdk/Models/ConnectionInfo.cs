e3a1ae6ab1c77571496298c051b584dcf48147b1
Unknown <raoliveira@stone.com.br>
2017-12-21 12:45:17 -0200

    Add summary

--- SftpWrapper.Sdk/Models/ConnectionInfo.cs
+++ SftpWrapper.Sdk/Models/ConnectionInfo.cs
@@ -1,5 +1,8 @@
 ﻿namespace SftpWrapper.Sdk.Models
 {
+    /// <summary>
+    /// Represents remote connection information class.
+    /// </summary>
     public class ConnectionInfo
     {
         public string UserName { get; }
@@ -7,6 +10,13 @@
         public string Host { get;}
         public int Port { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the SftpWrapper.Sdk.Models.ConnectionInfo
+        /// </summary>
+        /// <param name="username">Authentication username.</param>
+        /// <param name="password">Authentication password.</param>
+        /// <param name="host">Connection host.</param>
+        /// <param name="port">Connection port</param>
         public ConnectionInfo(string username, string password, string host, 
                              int port = 22)
         {
--- SftpWrapper.Sdk/Models/SftpFile.cs
+++ SftpWrapper.Sdk/Models/SftpFile.cs
@@ -1,12 +1,23 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace SftpWrapper.Sdk.Models
 {
+    /// <summary>
+    /// Represents the file informations for download and upload
+    /// </summary>
     public class SftpFile
     {
         public string SourcePath { get; }
         public string DestinationPath { get; }
 
+        /// <summary>
+        /// Initialize a new instance of the <see="SftpClient"/> class.
+        /// </summary>
+        /// <param name="sourcePath">Source path of the file</param>
+        /// <param name="destinationPath">Destination path of the file.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="sourcePath"/> is <b>null</b>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="destinationPath"/> is <b>null</b>.</exception>
         public SftpFile(string sourcePath, string destinationPath)
         {
             SourcePath = sourcePath;
