241388009a25e53593617f3dee0db7b46b681eb9
Unknown <raoliveira@stone.com.br>
2017-12-21 15:50:37 -0200

    Update class name

--- SftpWrapper.Sdk/Models/SftpFileInfo.cs
+++ SftpWrapper.Sdk/Models/SftpFileInfo.cs
@@ -6,7 +6,7 @@ namespace SftpWrapper.Sdk.Models
     /// <summary>
     /// Represents the file informations for download and upload
     /// </summary>
-    public class SftpFile
+    public class SftpFileInfo
     {
         public string SourcePath { get; }
         public string DestinationPath { get; }
@@ -18,12 +18,15 @@ namespace SftpWrapper.Sdk.Models
         /// <param name="destinationPath">Destination path of the file.</param>
         /// <exception cref="ArgumentNullException"><paramref name="sourcePath"/> is <b>null</b>.</exception>
         /// <exception cref="ArgumentNullException"><paramref name="destinationPath"/> is <b>null</b>.</exception>
-        public SftpFile(string sourcePath, string destinationPath)
+        public SftpFileInfo(string sourcePath, string destinationPath)
         {
             SourcePath = sourcePath;
             DestinationPath = destinationPath;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public string Name => Path.GetFileName(SourcePath);
     }
 }
