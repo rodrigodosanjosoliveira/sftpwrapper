0bfe6fdaf9311053156ce2beb436d04086ef0ad1
Rodrigo Anjos Oliveira <raoliveira@stone.com.br>
2018-01-05 19:15:43 -0200

    Update DownloadFromSftpTests.cs

--- SftpWrapper.Tests/DownloadFromSftpTests.cs
+++ SftpWrapper.Tests/DownloadFromSftpTests.cs
@@ -13,7 +13,7 @@ namespace SftpWrapper.Tests
         private const string User = "foo";
         private const string Password = "pass";
         private const string SourcePath = "/upload/";
-        private readonly string _destinationPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private readonly string _destinationPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
         private const int Port = 2222;
         private readonly ConnectionInfo _connection;
 
